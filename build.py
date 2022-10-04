0c67f8746a2793de611f201cded5ccdbe8103021
Justin Joyce <justin.joyce@joocer.com>
2022-10-05 00:30:01 +0100

    try again

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from setuptools_rust import RustExtension
 def build(setup_kwargs: Dict[str, Any]) -> None:
     setup_kwargs.update(
         {
-            "rust_extensions": [RustExtension("sqloxide.sqloxide", "Cargo.toml", debug=False)],
+            "rust_extensions": [RustExtension("sqlo2.sqlo2", "Cargo.toml", debug=False)],
             "zip_safe": False
         }
     )
